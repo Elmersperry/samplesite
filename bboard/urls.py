dabc69c3df0c06f60e26e3ffcbf299f777f27ef0
Elmersperry <rodionstudent@mail.ru>
2025-06-03 17:17:56 +0300

    ggggggg

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import index, about, add_bb, read_bb, update_bb, delete_bb, user_bbs, user_info, search_bb, filter_bb
 
 app_name = 'bboard'
@@ -13,4 +15,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('bb/user/info/<int:user_id>/', user_info, name='user_info'),
     path('bb/user/<int:user_id>/', user_bbs, name='user_bbs'),
+    path('403/', TemplateView.as_view(template_name='bboard/403.html')),
+    path('404/', TemplateView.as_view(template_name='bboard/404.html')),
+    path('500/', TemplateView.as_view(template_name='bboard/500.html')),
 ]
